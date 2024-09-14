2d2441a0a2c57eab4a02ca880dfd6b310dfb6e94
asjadahamed77 <ahamedasjad77@gmail.com>
2024-09-14 20:38:26 +0530

    Created Pages and Mounted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Collection from './pages/Collection';
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Orders from './pages/Orders';
+import PlaceOrder from './pages/PlaceOrder';
+import Product from './pages/Product';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <BrowserRouter>
+     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/orders' element={<Orders/>} />
+        <Route path='/place-order' element={<PlaceOrder/>} />
+        <Route path='/product/:productId' element={<Product/>} />
+      </Routes>
+     </div>
+     </BrowserRouter>
     </div>
   );
 }
