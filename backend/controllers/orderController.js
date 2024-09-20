2b645fe086896a1fbf78b1c1f15daed26667d4dd
asjadahamed77 <ahamedasjad77@gmail.com>
2024-09-20 08:37:42 +0530

    Updated Frontend Orders.jsx

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,9 +54,13 @@ const allOrders = async (req,res) =>{
 
 const userOrders = async (req,res) =>{
     try {
-        
+        const { userId } = req.body
+
+        const orders = await orderModel.find({userId})
+        res.json({success:true, orders})
     } catch (error) {
         console.log(error)
+        res.json({success:false, message:error.message})
     }
 }
 
